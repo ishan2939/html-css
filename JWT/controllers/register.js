1729a0554ecb681848df30e727d4c06c17e0e3e1
harkhanishan <harkhaniishan18@gmail.com>
2023-04-27 19:13:52 +0530

    [feat]: Added the JWT, session and cookies hands on

--- JWT/controllers/register.js
+++ JWT/controllers/register.js
@@ -5,9 +5,9 @@ const bcrypt = require('bcryptjs');
 exports.register = async (req, res) => {
 
     try{
-        const {name, email, password} = req.body;
+        const {name, email, password, role} = req.body;
 
-        if(!(name && email && password)){
+        if(!(name && email && password && role)){
             return res.status(400).send("Not sufficient data provided.");
         }
         
@@ -19,9 +19,10 @@ exports.register = async (req, res) => {
         encryptedPassword = await bcrypt.hash(password,10);
 
         const user = await User.create({
-            name: name,
+            fullName: name,
             email:  email.toLowerCase(),
-            password: encryptedPassword
+            password: encryptedPassword,
+            role: role
         });
 
         const token = jwt.sign(
