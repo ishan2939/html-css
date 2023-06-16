062180b4c2de659c3fd54452e2e247a3f78e9654
harkhanishan <harkhaniishan18@gmail.com>
2023-06-16 19:34:57 +0530

    feat: added validation in user schema

--- sequelize/models/user.js
+++ sequelize/models/user.js
@@ -1,6 +1,11 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 const User = Sequelize.define("user", {
+    u_id: {
+        type:  DataTypes.INTEGER,
+        primaryKey: true,
+        AutoIncrement: true
+    },
 
     fname: {
         type: DataTypes.STRING(20),
@@ -9,15 +14,37 @@ const User = Sequelize.define("user", {
             notEmpty: true,
         },
     },
+
     lname: {
         type: DataTypes.STRING(20),
         defaultValue: "",
     },
+
+    fullName: {
+        type: DataTypes.VIRTUAL,
+        get(){
+            return this.getDataValue('fullName');
+        },
+        set(){
+            const fname = this.getDataValue('fname');
+            const lname = this.getDataValue('lname');
+
+            const fullName = fname[0].toUpperCase() + fname.slice(1) + ' ' +
+                lname[0].toUpperCase() + lname.slice(1);
+            
+            this.setDataValue('fullName', fullName);
+        }
+    },
+
     email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        validate: {
+            isEmail: true
+        }
     },
+
     password: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -25,6 +52,7 @@ const User = Sequelize.define("user", {
             len: [8, "Infinity"],
         },
     },
+
     gender: {
         type: DataTypes.STRING,
         defaultValue: "male",
@@ -32,16 +60,9 @@ const User = Sequelize.define("user", {
             isIn: [["male", "female"]],
         },
     },
+
     b_date: {
         type: DataTypes.DATE,
         validate: {},
-    },
-    address: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        },
     }
-    
 });
