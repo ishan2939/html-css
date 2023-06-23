a7b34f504cde0e55a0f9a54510a03448e5c92720
harkhanishan <harkhaniishan18@gmail.com>
2023-06-23 19:24:11 +0530

    feat: practice types, interface and functions

--- practice/build/js/index.js
+++ practice/build/js/index.js
@@ -1,5 +1,49 @@
 "use strict";
+var _a;
 let x = 10;
 let y = 11;
 let z = 12;
 console.log(x + y + z);
+let a = "9";
+a = "10";
+let b = 2;
+let c = [10, 20];
+let d = ["10", true];
+let e = [2, "hello"];
+let f = [2, "hi"];
+let g = ["hello", 3];
+let h = ["hello", true, 33, 2, false];
+let i = {
+    name: "ishan",
+    age: 22,
+};
+let j = {
+    quantity: 3,
+};
+console.log((_a = j.icon) === null || _a === void 0 ? void 0 : _a.toUpperCase());
+let k;
+let l;
+l = "jeel";
+const m = (x, y) => {
+    console.log(x + y);
+};
+const n = (x, y) => {
+    return x + y;
+};
+const o = (a, ...b) => {
+    return a + b.reduce((prev, next) => prev + next);
+};
+o(2, 3);
+o(2, 3, 6, 7, 8);
+const p = function (a, b, c) {
+    if (typeof c != 'undefined')
+        return a + b + c;
+    return a + b;
+};
+const q = (a, b = 2, c = 3) => {
+    return a + b + c;
+};
+q(1);
+q(1, 3);
+q(1, 3, 4);
+q(1, undefined, 5);
--- practice/src/index.ts
+++ practice/src/index.ts
@@ -12,38 +12,73 @@ let b: string | number = 2;
 let c: number[] = [10, 20];
 let d: (string | boolean)[] = ["10", true];
 
-let e:[number, string] = [2,'hello'];
+let e: [number, string] = [2, "hello"];
 
-let f = [2, 'hi'];
+let f = [2, "hi"];
 
 //e = f;
 
-
 type myType1 = [string, number];
 
-let g:myType1 = ['hello', 3];
+let g: myType1 = ["hello", 3];
 
 type myType2 = (string | boolean | number)[];
 
-let h:myType2 = ['hello', true, 33, 2, false];
+let h: myType2 = ["hello", true, 33, 2, false];
 
 type myType3 = {
-    name: string,
-    age: number
+  name: string;
+  age: number;
 };
 
-let i:myType3 = {
-    name: 'ishan',
-    age: 22
-}
+let i: myType3 = {
+  name: "ishan",
+  age: 22,
+};
 
 type myType4 = {
-    icon?: string,
-    quantity: number
+  icon?: string;
+  quantity: number;
 };
 
-let j:myType4 = {
-    quantity: 3
+let j: myType4 = {
+  quantity: 3,
+};
+
+console.log(j.icon?.toUpperCase());
+
+let k: "ishan";
+
+let l: "ishan" | "jeel" | "parth";
+
+l = "jeel";
+
+const m = (x: number, y: number) => {
+  console.log(x + y);
+};
+
+const n = (x: number, y: number): number => {
+  return x + y;
+};
+
+const o = (a: number, ...b: number[]): number => {
+    return a + b.reduce((prev, next) => prev+next);
+};
+
+o(2, 3);
+o(2,3,6,7,8);
+
+const p = function(a:number, b:number, c?:number):number {
+    if(typeof c != 'undefined')
+        return a+b+c;
+    return a+b;
 }
 
-console.log(j.icon?.toUpperCase())
+const q = (a:number, b:number = 2, c:number = 3)=> {
+    return a+b+c;
+};
+
+q(1);
+q(1,3);
+q(1,3,4);
+q(1, undefined, 5);
